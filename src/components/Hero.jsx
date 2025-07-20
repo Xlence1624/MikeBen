31a128ac5bb1891f4fa632f8e40a22d41129eee9
Xlence1624 <xlence1624@gmail.com>
2025-07-20 01:50:06 +0100

    padding adjustments
    Signed-off-by: Xlence1624 <xlence1624@gmail.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,45 @@
-import React from 'react'
-import Nav from './Nav'
-import DottedSquare from './DottedSquare'
-import img from '../assets/011.JPG'
-
-
+import React from "react";
+import Nav from "./Nav";
+import DottedSquare from "./DottedSquare";
+import img from "../assets/011.JPG";
 
 function Hero() {
   return (
-    <div className=' flex  flex-col md:flex-row '>
- <Nav/>
-<div className=' bg-secondary md:w-2/3  text-primary md:h-3/4'>
-
-
-<div className='   pt-48 px-29 pb-5   '>
-<p className='font-[900] text-[45px] mt-5 leading-12 md:text-6xl md:leading-15'>Fullstack <br />Developer<span className='text-white'>.</span></p>
-<p className='text-white mt-3 text-[18px]'>I craft scalable and user friendly web apps with  great user experiences. </p>
-<div className='flex  gap-8 text-[10px] md:text-[13px] mt-20   pb-3 '>
-  <p>
-    Highly skilled at building responsive <br />and interactive web applications using <br /> modern technologies.
-  </p>
-  <p>Proven experience building  <br />successful products for clients <br />across several countries.</p>
-</div>
-</div>
-
-</div>
-<div className='bg-primary p-27 md:w-1/3 '>
-<div className='p-5'>
-<DottedSquare/>
-</div>
-<div className='p-5 absolute left-3/5 top-15/16 md:top-3/5 md:left-2/3'>
-<DottedSquare/>
-</div>
-
-
-
-<div className=' absolute left-1/4 top-5/5 md:left-12/21 md:top-1/5'>
-<img src={img} alt="" className=' w-[230px] h-[220px] md:h-[320px] md:w-[280px] object-cover object-top md:rounded-2xl'  />
-</div>
-
-</div>
-
-
+    <div className=" flex  flex-col md:flex-row ">
+      <Nav />
+      <div className=" bg-secondary md:w-2/3  text-primary md:h-3/4">
+        <div className="   pt-25 md:pt-48 md:px-29 pb-5 flex flex-col justify-center items-center  ">
+          <p className="font-[900] text-[45px] mt-5 leading-12 md:text-6xl md:leading-15">
+            Fullstack <br />
+            Developer<span className="text-white">.</span>
+          </p>
+          <p className="text-white mt-3 text-[18px] text-center">
+            I craft scalable and user friendly web apps with great user
+            experiences.
+          </p>
+          <div className="flex  gap-4 text-[10px] md:text-[13px] mt-15 md:mt-20  px-3 pb-3 ">
+            <p>
+              Highly skilled at building responsive and interactive web
+              applications using modern technologies.
+            </p>
+            <p>
+              Proven experience building successful products for clients across
+              several countries.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-primary p-27 md:w-1/3 flex justify-center items-center ">
+        {/* <div className="  md:absolute  md:left-12/21 md:top-1/5"> */}
+          <img
+            src={img}
+            alt=""
+            className=" w-[230px] h-[230px]  md:h-[320px] md:w-[280px] object-cover object-top md:rounded-2xl"
+          />
+        {/* </div> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     
 
-        <div className='flex justify-between  absolute top-0 w-full px-20 pt-12 bg-transparent text-primary text-3xl'>
+        <div className='flex justify-between  absolute top-0 w-full p-5 md:px-20 md:pt-12 bg-transparent text-primary text-3xl'>
  <p className='  font-[900] '>Michael</p>
   <p className='leading-3  font-[900] text-right md:text-[#355edb]'>—– <br />—</p>
   </div>
