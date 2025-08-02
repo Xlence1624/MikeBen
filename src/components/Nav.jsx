05b7b83481f7d2720d6a17df385c45ed33d412f5
Xlence1624 <xlence1624@gmail.com>
2025-08-02 13:52:32 +0100

    fixed
    Signed-off-by: Xlence1624 <xlence1624@gmail.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
 
         <div className='flex justify-between  absolute top-0 w-full p-5 md:px-20 md:pt-12 bg-transparent text-primary text-3xl'>
  <p className='  font-[900] '>Michael</p>
-  <p className='leading-3  font-[900] text-right md:text-[#355edb] '> <span>—–</span> <br />—</p>
+  <p className='leading-3  font-[900] text-right md:text-[#355edb] '> <span className=''>—</span> <br />–</p>
 
   
 
