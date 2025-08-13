181424ed1c0fb5f9eaf1769932da81d45616f23a
Xlence1624 <xlence1624@gmail.com>
2025-08-13 15:17:41 +0100

    nav content edit
    Signed-off-by: Xlence1624 <xlence1624@gmail.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,18 +19,18 @@ const Nav = () => {
 
   </div>
 
-<div className={`  fixed h-screen w-full flex items-center  flex-col text-[#355edb]  md:absolute top-0  duration-1000 ease-in-out  shadow-lg rounded-lg  md:w-1/4 md:h-max bg-white  md:my-5 md:mx-10 md:p-10 ${ isOpen ? 'right-0' : ' -right-350'}`}>
+<div className={`  fixed h-screen w-full flex items-center justify-around flex-col text-[#355edb]  md:absolute top-0  duration-1000 ease-in-out  shadow-lg rounded-lg  md:w-1/4 md:h-max bg-white  md:my-5 md:mx-10 md:p-10 ${ isOpen ? 'right-0' : ' -right-350'}`}>
 <p className=' absolute right-2 top-0 text-3xl cursor-pointer' onClick={()=> setIsOpen(false)}
   
   >&#215;</p>
-  <ul className='fixed top-1/4 flex flex-col items-center justify-between gap-10 md:gap-6  text-xl lora'>
+  <ul className=' text-xl  gap-5    flex  flex-col items-center justify-between sm:gap-5 md:gap-6  md:text-xl lora md:static'>
     <li>Home</li>
     <li>My Work</li>
     <li>My Resume</li>
     <li>Contact me</li>
 
   </ul>
-<div  className='  flex flex-col justify-between gap-5 bottom-10 md:gap-4 text-center absolute md:static  md:mt-8'>
+<div  className=' flex flex-col gap-1  md:gap-4 text-center  md:static  md:mt-8'>
     <p className='text-gray-300 '>SAY HELLO</p>
   <p>orjimichael73@yahoo.com</p>
   <div className='flex justify-center items-center  gap-4  text-2xl '>
