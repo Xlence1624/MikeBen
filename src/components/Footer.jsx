5d68b18ce0e015624fd10b1d4eeecd87daccf984
Xlence1624 <xlence1624@gmail.com>
2025-08-15 01:47:05 +0100

    footer
    Signed-off-by: Xlence1624 <xlence1624@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,16 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex flex-col justify-between 
-    bg-purple-950 text-primary p-8 md:p-20 gap-10'>
+    bg-purple-950 text-primary p-8 md:pt-40 gap-10'>
 <div className='flex justify-around '>
 
-  <div>
-      <p>SAY HELLo</p>
-<p>orjimichael73@yahoo.com</p>
-<p>Whatsapp</p>
 
 
-  </div>
-
-  <div>
+  
     <p>My work</p>
     <p>My contact</p>
     <p>My Resume</p>
-  </div>
+
 
 </div>
 
@@ -26,12 +20,24 @@ const Footer = () => {
 
 <div>
   <hr />
-  <div className='flex justify-between p-5'>
+  {/* <div className='flex justify-between p-5'>
 
     <p>© 2023 Orji Michael. All rights reserved.</p>
 
     <p>TW   GH   LN  YT</p>
-  </div>
+  </div> */}
+  <div  className=' flex flex-col  justify-around gap-1 md:gap-4 text-center  md:static  md:mt-8'>
+    <p className='text-gray-300 '>SAY HELLO</p>
+  <p>orjimichael73@yahoo.com</p>
+  <div className='flex justify-center items-center  gap-4  text-2xl '>
+<i class="fa-brands fa-github"></i>
+<i class="fa-brands fa-square-linkedin"></i>
+<i class="fa-brands fa-whatsapp"></i>
+<i class="fa-brands fa-x-twitter"></i>
+  
+</div>
+</div>
+  <p className='text-center text-gray-300 mt-10'>© 2025 Orji Michael. All rights reserved.</p>
 </div>
 
     </div>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -46,9 +46,12 @@ to say hello? Go ahead.</p>
     <input type="text" name="email" onChange={handleChange}placeholder='Enter your Email address' className='border-b-1 border-purple-900/50 w-80 py-2 outline-0' />
   </div>
 </div>
- <textarea name="message" onChange={handleChange} id=" "  placeholder='Write your message here.' className='border-b-1 border-purple-900/50 md:w-1/2  w-full  text-purple-950 outline-0  my-10' ></textarea>
+<div className='flex flex-col justify-center items-center mt-10'>
+ <textarea name="message" onChange={handleChange} id=" "  placeholder='Write your message here.' className='border-b-1 border-purple-900/50 md:w-1/2  w-full  text-purple-950 outline-0  my-10 text-center' ></textarea>
 
  <button type="submit" className='border-s-1 border bg-secondary text-white p-2 cursor-pointer' >Send message</button>
+</div>
+
 </form>
 
 
