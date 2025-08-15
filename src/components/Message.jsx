a4c1282a2f4771f51d58c1ed4676965fef68137d
Xlence1624 <xlence1624@gmail.com>
2025-08-15 01:08:14 +0100

    form
    Signed-off-by: Xlence1624 <xlence1624@gmail.com>

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -33,6 +33,8 @@ const sendEmail = (e) => {
 to say hello? Go ahead.</p>
 
 </div >
+
+<form action="" onSubmit={sendEmail}>
 <div className='flex flex-col md:flex-row justify-between gap-20 mt-20 text-purple-950  ' >
   <div>
     <p className='text-gray-300 mb-2 text-sm'>Your Name</p>
@@ -44,10 +46,14 @@ to say hello? Go ahead.</p>
     <input type="text" name="email" onChange={handleChange}placeholder='Enter your Email address' className='border-b-1 border-purple-900/50 w-80 py-2 outline-0' />
   </div>
 </div>
- 
  <textarea name="message" onChange={handleChange} id=" "  placeholder='Write your message here.' className='border-b-1 border-purple-900/50 md:w-1/2  w-full  text-purple-950 outline-0  my-10' ></textarea>
 
- <p type="submit" className='border-s-1 border bg-secondary text-white p-2 cursor-pointer' onClick={sendEmail}>Send message</p>
+ <button type="submit" className='border-s-1 border bg-secondary text-white p-2 cursor-pointer' >Send message</button>
+</form>
+
+
+ 
+
     </div>
   )
 }
